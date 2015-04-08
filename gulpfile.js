55bef2c3cae86bab074856f2c28d6686817f4c84
vvlasov <vasilyp.vlasov@gmal.com>
2015-04-08 17:46:46 +0300

    create base tasks

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// Include gulp !important
+var gulp = require('gulp');
+
+// Include Our Plugins here
+var jshint = require('gulp-jshint');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var myth = require('gulp-myth');
+var csso = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var connect = require('gulp-connect');
+var nib = require('nib');
+var rimraf = require('gulp-rimraf');
+
+// jade task
+gulp.task('jade', function() {
+    gulp.src(['./source/**/*.jade', './source/**/_*.jade'])
+        .pipe(jade({
+            pretty: true
+        }))  
+        .on('error', console.log)
+    .pipe(gulp.dest('./dist/')) 
+    .pipe(connect.reload());
+});
+
+// stylus task
+gulp.task('stylus', function() {
+    gulp.src(['./source/**/*.styl', './source/**/_*.styl'])
+        .pipe(stylus({
+            use: [nib()]
+        })) 
+    .on('error', console.log) 
+    .pipe(myth())
+    .pipe(gulp.dest('./dist/')) 
+    .pipe(connect.reload());
+});
+
+// js task
+gulp.task('js', function() {
+    gulp.src(['./source/**/*.js', '!./source/**/_*.js'])
+        .pipe(gulp.dest('./public/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('connectDev', function () {
+  connect.server({
+    root: ['dist'],
+    port: 3000,
+    livereload: true
+  });
+});
+
+
+// watch task
+gulp.task('watch', function() {
+    gulp.watch('./source/**/*.styl', ['stylus']);
+    gulp.watch('./source/**/*.jade', ['jade']);
+    gulp.watch('./source/**/*.js', ['js']);
+});
+
+gulp.task('default', ['jade', 'stylus', 'js', 'connectDev', 'watch']);
